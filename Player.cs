72b882500db73d7b58f47fde43d2402037771800
Hebrin <ashram28@gmail.com>
2020-09-07 00:43:11 +0300

    Рабочая версия с полным функционалом 1.0

--- Player.cs
+++ Player.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 
 namespace RPS
 {
 	public class Player
 	{
 		public string name;
-		public variants element;
-		
-		public Player()  //Задаем конструктор случайного значения
+		public variants element; 
+					
+		public Player()						//Задаем конструктор случайного значения
         {
-			Random rnd = new Random();     //Создание объекта для генерации чисел
+			Random rnd = new Random();      //Создание объекта для генерации чисел
 			int value = rnd.Next(0, 2);     //Получаем индекс значения варианта (в диапазоне от 0 до 2)
 			element = (variants)value;
 			name = "bot";
@@ -21,26 +22,43 @@ namespace RPS
 			
 		}
 
-
-        public string whoWins(Player bot, Player alex) //Функция вывод победителя
+		private string getVariantName(variants variant)     //Создаем метод для преобразования значений на русский язык
         {
-			if (bot.element == alex.element)			//Проверка на ничью
+			switch (variant)								//Переводим значения на русский язык для вывода пользователю
             {
-				return "Ничья";
+				case variants.paper:
+					return "бумага";
+				case variants.rock:
+					return "камень";
+				case variants.scissors:
+					return "ножницы";
+				default:
+					return "";
+
+            }
+				 
+        }
+
+        public string whoWins(Player bot, Player gamer)		//Метод вывод победителя
+        {
+			if (bot.element == gamer.element)				//Проверка на ничью
+            {
+				return $"У противника выпал {getVariantName(bot.element)} Ничья";
 
 			}
-			string winner;								//Переменная winner в которой будет победитель 
-			if (bot.element == variants.scissors)		//Поиск победителя
+			string winner;																	 //Переменная winner в которой будет победитель 
+			if ( (bot.element == variants.paper && (gamer.element == variants.rock)) ||		 //Поиск победителя если нет ничьи
+				(bot.element == variants.rock && (gamer.element == variants.scissors)) ||
+				(bot.element == variants.scissors && (gamer.element == variants.paper)) )			
             {
 				winner = bot.name;
-            }
-			else
-            {
-				winner = alex.name;
-            }
-											
-											
-            return "Победил игрок с именем: " +winner;
+			}
+			else 
+			{
+				winner = gamer.name;
+			}
+			
+			return $"У противника выпал {getVariantName(bot.element)} Победил игрок с именем: " +winner;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Threading;
+
+
 
 namespace RPS
 {
@@ -7,10 +8,53 @@ namespace RPS
     {
         static void Main(string[] args)
         {
-            Player bot = new Player();
-            Player alex = new Player(variants.paper, "Alex");
+            Console.WriteLine("Введите своё имя:");         //Запрашиваем имя пользователя
+            string inputName = Console.ReadLine();
+            
+            while (true)                                    //Запускаем цикл для повторной игры
+            {
 
-            Console.Write(bot.whoWins(bot, alex));
+                //Предлагаем сыграть и получаем значение от пользователя
+                Console.WriteLine($"Привет {inputName}. Сыграем?! Введите один из вариантов (камень, ножницы или бумага) или выход");
+                string inputEl = Console.ReadLine();
+                variants playerVariant;
+                //Проверяем совпадает ли введенное значение с заданными и присваиваем в случае успеха
+                if (inputEl == "бумага")
+                {
+                    playerVariant = variants.paper;
+                }
+                else if (inputEl == "камень")
+                {
+                    playerVariant = variants.rock;
+                }
+                else if (inputEl == "ножницы")
+                {
+                    playerVariant = variants.scissors;
+                }
+                else if (inputEl == "выход")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверное значение");
+                    break;
+                }
+                //создаем объекты игрока и бота
+                Player gamer = new Player(playerVariant, inputName);
+                Player bot = new Player();
+
+
+                Console.WriteLine(bot.whoWins(bot, gamer));            //определяем победителя
+                Console.WriteLine("Хочешь сыграть ещё? (да/нет)");      //предлагаем сыграть ещё раз
+                string playAgain = Console.ReadLine();
+                if (playAgain != "да")                                  //Прерываем цикл если пользователь отказывается
+                {
+                    break;
+                }
+
+
+            }
         }
     }
 }
